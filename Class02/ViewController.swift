bba2760dd69225ed77014600442b2670c6c21988
張雅雯 <wen77797@gmail.com>
2020-03-31 16:36:13 +0800

    initial project

---
+++ Class02/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Class02
+//
+//  Created by chang on 2020/3/31.
+//  Copyright © 2020 chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lbmsg: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnclick(_ sender: Any) {
+        self.lbmsg.text = "世界好"
+    }
+    
+}
+
